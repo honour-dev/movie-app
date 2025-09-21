431c35a6cfa6f937a677aec4505ff9bd05e7fd4b
Obayuwana Honour Okeoghene <honouroke@gmail.com>
2025-09-21 23:18:00 +0100

    navbar correction

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className='bg-lime-700 w-full md:px-[5rem]'>
       <div className='flex justify-between p-3'>
-          <div className='cursor-pointer text-lime-100 uppercase font-bold'>
-            <Link to='/'> movie app</Link>
-          </div>
-          <div className='font-medium'>
-            <Link to='/' className='border-lime-700 pl-3 pr-3 p-1 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200'>Home</Link>
-            <Link to='/favourite' className='border-lime-700 ml-0.5 px-3 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200'>Favourites</Link>
-          </div>
+        <div className='cursor-pointer text-lime-100 uppercase font-bold'>
+          <NavLink to='/'> movie app</NavLink>
+        </div>
+        <div className='font-medium'>
+          <NavLink to='/'
+            className={({ isActive }) =>
+              `border-lime-700 px-3 p-1 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 ${
+                isActive ? 'bg-lime-200 text-lime-700' : ''
+              }`
+            }
+          > Home </NavLink>
+
+          <NavLink to='/favourite'
+            className={({ isActive }) =>
+              `border-lime-700 ml-0.5 px-3 p-1 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 ${
+                isActive ? 'bg-lime-200 text-lime-700' : ''
+              }`
+            }
+          > Favourites </NavLink>
+        </div>
       </div>
     </nav>
   )
---
+++ src/trash.jsx
@@ -0,0 +1,7 @@
+{<div className='cursor-pointer text-lime-100 uppercase font-bold'>
+            <Link to='/'> movie app</Link>
+          </div>
+          <div className='font-medium'>
+            <Link to='/' className='border-lime-700 pl-3 pr-3 p-1 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 active:text-lime-700 active:bg-lime-200'>Home</Link>
+            <Link to='/favourite' className='border-lime-700 ml-0.5 px-3 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 active:text-lime-700 active:bg-lime-200'>Favourites</Link>
+</div>}

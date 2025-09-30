967379615129620acadbb8eeb7e0c6431d44697b
Obayuwana Honour Okeoghene <honouroke@gmail.com>
2025-10-01 00:52:50 +0100

    navbar style correction

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
         <div className='font-medium'>
           <NavLink to='/'
             className={({ isActive }) =>
-              `px-3 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
+              `px-3 pt-1 pb-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
           > Home</NavLink>
 
           <NavLink to='/favourite'
             className={({ isActive }) =>
-              `ml-0.5 px-3 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
+              `ml-0.5 px-3 pt-1 pb-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
           > Favourites</NavLink>
         </div>
       </div>

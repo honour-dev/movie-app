e1e80bbf78225f8048f2737e4d833468012540f5
Obayuwana Honour Okeoghene <honouroke@gmail.com>
2025-10-01 00:57:34 +0100

    final commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -49,8 +49,8 @@ const HomePage = () => {
         <div className="absolute inset-0 z-1" style={{background: "linear-gradient(to bottom, white 10%, transparent 30%)"}}></div>
         <div className='absolute inset-0 bg-black opacity-60'></div>
         <div className='flex flex-col items-center px-4 py-[10rem] mb-[2rem] z-1 relative'>
-          <h2 className='text-lime-200 text-3xl text-center font-extrabold'>Unlimited movies, TV shows, and more.</h2>
-          <p className='text-lime-200'>Ready to watch? Enter movie name below to search and enjoy...</p>
+          <h2 className='text-lime-200 text-3xl text-center font-extrabold mb-2'>Unlimited movies, TV shows, and more.</h2>
+          <p className='text-lime-200 text-center'>Ready to watch? Enter movie name below to search and enjoy...</p>
           <form onSubmit={handleSearch} className='mb-4 mt-4 w-full max-w-xl flex justify-center'>
             <input 
               type="text" 

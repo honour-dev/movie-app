edb6ebf59f38b813d3cb07c743dd8d2defc1799c
Obayuwana Honour Okeoghene <honouroke@gmail.com>
2025-10-01 00:43:56 +0100

    final push

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,19 +4,19 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='bg-lime-700 w-full md:px-[5rem]'>
-      <div className='flex justify-between p-3'>
-        <div className='cursor-pointer text-lime-100 uppercase font-bold'>
-          <NavLink to='/'> movie app</NavLink>
+      <div className='flex justify-between p-3 md:text-xl'>
+        <div className='cursor-pointer text-lime-100 md:text-2xl capitalize font-bold'>
+          <NavLink to='/'>honour flix</NavLink>
         </div>
         <div className='font-medium'>
           <NavLink to='/'
             className={({ isActive }) =>
-              `px-3 p-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 ${isActive ? 'bg-lime-200 text-lime-700' : 'text-white'}`}
+              `px-3 pb-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
           > Home</NavLink>
 
           <NavLink to='/favourite'
             className={({ isActive }) =>
-              `ml-0.5 px-3 p-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 ${isActive ? 'bg-lime-200 text-lime-700' : 'text-white'}`}
+              `ml-0.5 px-3 pb-1 cursor-pointer hover:bg-lime-200 hover:text-lime-700 hover:rounded-4xl duration-200 ${isActive ? 'bg-lime-200 text-lime-700 rounded-4xl' : 'text-white'}`}
           > Favourites</NavLink>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
   const handleSearch = async (e) =>{
     e.preventDefault();
     if(!searchQuery.trim()) return;
-    if(loading) return;
+    // if(loading) return;
 
     setLoading(true)
     try {
@@ -37,27 +37,35 @@ const HomePage = () => {
     } catch (err) {
       console.log(err);
       setError('failed to load search results...');
+    } finally {
+      setLoading(false);
     }
     setSearchQuery('');
   }
 
   return (
-    <main className='md:px-[5rem] px-3'>
-      <section className='flex flex-col items-center px-4'>
-        <form onSubmit={handleSearch} className='mb-4 mt-4 w-full max-w-xl flex justify-center'>
-        <input 
-          type="text" 
-          placeholder='search for movies...' 
-          value={searchQuery} 
-          onChange={(e) => setSearchQuery(e.target.value)} 
-          className='p-1 pl-2 border-2 border-lime-700 w-full'
-        />
-        <button 
-          type='submit' 
-          className='border-lime-700 border-2 ml-0.5 pl-3 pr-3 p-1 bg-lime-700 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200'>
-            search
-        </button>
-      </form>
+    <main className='md:px-[5rem] px-4'>
+      <section className='relative bg-center bg-cover h-full w-full' style={{backgroundImage: "url(/movieBackgroundImage.jpg)"}}>
+        <div className="absolute inset-0 z-1" style={{background: "linear-gradient(to bottom, white 10%, transparent 30%)"}}></div>
+        <div className='absolute inset-0 bg-black opacity-60'></div>
+        <div className='flex flex-col items-center px-4 py-[10rem] mb-[2rem] z-1 relative'>
+          <h2 className='text-lime-200 text-3xl text-center font-extrabold'>Unlimited movies, TV shows, and more.</h2>
+          <p className='text-lime-200'>Ready to watch? Enter movie name below to search and enjoy...</p>
+          <form onSubmit={handleSearch} className='mb-4 mt-4 w-full max-w-xl flex justify-center'>
+            <input 
+              type="text" 
+              placeholder='search for movies...' 
+              value={searchQuery} 
+              onChange={(e) => setSearchQuery(e.target.value)} 
+              className='p-1 pl-2 border-2 border-lime-200 w-full placeholder:text-lime-200 text-lime-200 outline-lime-200'
+            />
+            <button 
+              type='submit' 
+              className='border-lime-200 border-2 ml-0.5 pl-3 pr-3 p-1 bg-lime-700 text-white cursor-pointer hover:bg-lime-200 hover:text-lime-700 duration-200 font-medium'>
+                search
+            </button>
+          </form>
+        </div>
       </section>
  
       {loading ? <Loading/> :
